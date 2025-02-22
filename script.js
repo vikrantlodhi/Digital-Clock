4d6e46475aa9aa5cf6c01ae9b6e743ea1846de90
Vikrant Lodhi <161944512+vikrantlodhi@users.noreply.github.com>
2025-02-22 22:18:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+        function updateClock() {
+            let now = new Date();
+            let hours = now.getHours().toString().padStart(2, '0');
+            let minutes = now.getMinutes().toString().padStart(2, '0');
+            let seconds = now.getSeconds().toString().padStart(2, '0');
+            let timeString = `${hours}:${minutes}:${seconds}`;
+            document.getElementById("clock").innerText = timeString;
+            
+            let dateString = now.toDateString();
+            document.getElementById("date").innerText = dateString;
+        }
+        setInterval(updateClock, 1000);
+        updateClock();
+    
